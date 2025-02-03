8b52c8cb3d691665b9050c1a59c7a9f9b656d265
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-02-03 07:01:38 +0700

    Revert "Revert "AGMIPC@1.0-impl/libagm: exclude from OSS builds""
    This reverts commit db92248be214f30d990b226324dd7750ea6cbc92.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- service/Android.mk
+++ service/Android.mk
@@ -6,6 +6,8 @@ LOCAL_VENDOR_MODULE         := true
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc/public
 include $(BUILD_HEADER_LIBRARY)
 
+ifneq ($(QCPATH),)
+
 # Build libagm
 include $(CLEAR_VARS)
 
@@ -71,4 +73,6 @@ LOCAL_HEADER_LIBRARIES += libaudiologutils_headers
 endif
 
 include $(BUILD_SHARED_LIBRARY)
+
+endif
 
